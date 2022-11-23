5b26f8246973667c741b19ad17a20161d9e9c49a
neortiz1 <neortiz1@espe.edu.ec>
2022-11-22 20:36:36 -0500

    First Commit

---
+++ NICOLAS_ESTEBAN_ORTIZ_ADAME/Geometry/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Geometry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Rectangle , Square, Circle
+            //Area, Perimeter
+            //Abstraction
+            //Encupsulation
+            //Inheritance
+            //Polymorphisn
+
+        }
+    }
+}
